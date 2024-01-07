f25b51b10ac3b367a5c9d1d6f0eba87bc426d6ce
Kotek506 <leonidias560@gmail.com>
2024-01-07 18:56:11 +0100

    homework day 6

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ChallengeApp;
+
+
+Employee user1 = new Employee("Andrew", "Golara", 56);
+Employee user2 = new Employee("Professional", "Fajer", 50);
+Employee user3 = new Employee("John", "Doe", 24);
+
+
+user1.AddScore(5);
+user1.AddScore(2);
+user1.AddScore(3);
+user1.AddScore(8);
+user1.AddScore(6);
+var result1 = user1.Result;
+// Console.WriteLine(result1);
+
+user2.AddScore(4);
+user2.AddScore(2);
+user2.AddScore(5);
+user2.AddScore(6);
+user2.AddScore(9);
+var result2 = user2.Result;
+// Console.WriteLine(result2);
+
+user3.AddScore(8);
+user3.AddScore(9);
+user3.AddScore(10);
+user3.AddScore(6);
+user3.AddScore(4);
+var result3 = user3.Result;
+// Console.WriteLine(result3);
+
+
+if (result1 > result2 && result1 > result3)
+{
+    Console.WriteLine("Najwyzszy wynik ma:");
+    Console.WriteLine("Imie: " + user1.Name);
+    Console.WriteLine("Nazwisko: " + user1.Surname);
+    Console.WriteLine("Wiek: " + user1.Age);
+    Console.WriteLine("Wynik: " + result1);
+}
+else if (result2 > result1 && result2 > result3)
+{
+    Console.WriteLine("Najwyzszy wynik ma:");
+    Console.WriteLine("Imie: " + user2.Name);
+    Console.WriteLine("Nazwisko: " + user2.Surname);
+    Console.WriteLine("Wiek: " + user2.Age);
+    Console.WriteLine("Wynik: " + result2);
+}
+else if (result3 > result1 && result3 > result2)
+{
+    Console.WriteLine("Najwyzszy wynik ma:");
+    Console.WriteLine("Imie: " + user3.Name);
+    Console.WriteLine("Nazwisko: " + user3.Surname);
+    Console.WriteLine("Wiek: " + user3.Age);
+    Console.WriteLine("Wynik: " + result3);
+}
---
+++ ChallengeApp/ChallengeApp/User.cs
@@ -0,0 +1,54 @@
+﻿//namespace ChallengeApp
+//{
+//    public class User
+//    {
+
+//        public static string GameName = "Diablo";
+//        private List<int> score = new List<int>();
+
+//        private string login;
+//        private string password;
+//        // private string name;
+
+//        //// public User(string login)
+//        // {
+//        //     this.login = login;
+//        //     this.password = "-";
+//        //     this.name = "-";
+//        // }
+//        // public User(string login, string password)
+//        // {
+//        //     this.login = login;
+//        //     this.password = password;
+//        //     this.name = "-";
+//        // }
+//        // public User()
+//        // {
+//        //     this.login = "-";
+//        //     this.password = "-";
+//        //     this.name = "-";
+//        // }
+//        public User(string login, string password)
+//        {
+//            this.Login = login;
+//            this.Password = password;
+//        }
+
+//        public string Login { get; private set; }
+
+//        public string Password { get; private set; }
+
+//        public int Result
+//        {
+//            get
+//            {
+//                return this.score.Sum();
+//            }
+//        }
+
+//        public void AddScore(int number)
+//        {
+//            this.score.Add(number);
+//        }
+//    }
+//}
