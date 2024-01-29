0724b00484b3bb1358b9b5c4d685c6eb89d97aa4
Kotek506 <leonidias560@gmail.com>
2024-01-29 14:37:36 +0100

    Homework day 12 + tests

--- ChallengeApp/ChallengeApp.Tests/EmployeeTests.cs
+++ ChallengeApp/ChallengeApp.Tests/EmployeeTests.cs
@@ -7,18 +7,22 @@ namespace ChallengeApp.Tests
         public void CheckEmployeeStatistics()
         {
             //arrange
-            var employee = new Employee("john", "cena");
-            employee.AddGrade(5);
-            employee.AddGrade(6);
-            employee.AddGrade(10);
+            var employee = new Employee();
+            employee.AddGrade('B');
+            employee.AddGrade('E');
+            employee.AddGrade("50");
+            employee.AddGrade(50);
+            employee.AddGrade("C");
+
 
             // act
             var statistics = employee.GetStatistics();
 
             // assert
-            Assert.AreEqual(statistics.Max, 10);
-            Assert.AreEqual(statistics.Min, 5);
-            Assert.AreEqual(statistics.Average, 7);
+            Assert.AreEqual(statistics.Max, 80);
+            Assert.AreEqual(statistics.Min, 20);
+            Assert.AreEqual(statistics.Average, 52);
+            Assert.AreEqual(statistics.AverageLetter, 'C');
         }
     }
 }
